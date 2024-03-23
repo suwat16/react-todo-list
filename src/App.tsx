86922c3b91918ecc596b68c90077498723c59e6e
suwat16 <suwat16128@gmail.com>
2024-03-23 11:43:36 +0700

    Add todo list functionality to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,37 @@
-import "./App.css";
+import React, { useState } from "react";
 
 function App() {
-  return <></>;
+  const [todoInput, setTodoInput] = useState<string>("");
+  const [todoList, setTodoList] = useState<string[]>([]);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    setTodoList([...todoList, todoInput]);
+  };
+
+  return (
+    <>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>Todo List</label>
+          <br />
+          <input
+            type="text"
+            value={todoInput}
+            onChange={(e) => setTodoInput(e.target.value)}
+          />
+
+          <button type="submit">Add</button>
+        </form>
+        <ul>
+          {todoList.map((todo, index) => (
+            <li key={index}>{todo}</li>
+          ))}
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default App;
