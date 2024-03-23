caf869e8edc4999e3c9e115372675685721a5935
suwat16 <suwat16128@gmail.com>
2024-03-23 14:33:09 +0700

    Refactor todo list rendering and add edit/delete buttons

--- src/components/todo-list.tsx
+++ src/components/todo-list.tsx
@@ -1,8 +1,12 @@
 const TodoList = ({ todoList }: { todoList: string[] }) => {
   return (
     <div>
-      {todoList.map((todo, index) => (
-        <li key={index}>{todo}</li>
+      {todoList.map((todoValue, index) => (
+        <div key={index + 1} >
+            {todoValue}
+            <button onClick={() => console.log('Edit')}>Edit</button>
+            <button onClick={() => console.log('Delete')}>Delete</button>
+        </div>
       ))}
     </div>
   );
